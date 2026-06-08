eaf9a067d41996801b86d81426a71ab779d5feeb
Aarav Vishesh Dewangan <aarav@Mac.lan>
2026-06-08 17:53:56 -0500

    add promo banner to homepage

--- app/page.tsx
+++ app/page.tsx
@@ -11,6 +11,7 @@ import Footer from "@/components/Footer";
 export default function Home() {
   return (
     <main>
+      <img src="/promo-banner.jpg" alt="Enroll Now - $500 off tuition" className="w-full" />
       <TopInfoBar />
       <HeaderCTA />
       <Navigation />
